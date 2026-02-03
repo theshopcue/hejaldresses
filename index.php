7ab8c4418da2f759a61b8acfee979da8d6e8ece0
theshopcue <theshopcue@gmail.com>
2026-02-03 14:37:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -154,6 +154,13 @@
     }, 1000);
   }
 </script>
+   <!-- Privacy-friendly analytics by Plausible -->
+<script async src="https://plausible.io/js/pa-GboqfQQNx_qNSpU5uKUeY.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>
+
 
 
 
